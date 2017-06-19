b1892f11211bd4bdc8aba5275f5904426868aee5
IanPan216 <xanadu0216@gmail.com>
2017-06-19 22:56:34 +0800

    init Project

---
+++ ShoppingCar/ShoppingCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCar
+{
+    public class Goods
+    {
+        public int Id { get; set; }
+        public Double SellPrice { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ ShoppingCarTest/ShoppingCarTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ShoppingCar;
+
+namespace ShoppingCarTest
+{
+    [TestClass]
+    public class ShoppingCarTest
+    {
+        [TestMethod]
+        public void 第一集買1本其他都沒買_價格應為100()
+        {
+            //arrange
+            var target = new ShoppingList();
+            var goods = GetGoods();
+            var excepted = 100;
+
+            //act
+            var actual = target.GetTotalAmount(goods);
+
+            //assert
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
